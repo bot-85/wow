47dcd4d00287ce8e2c549da6e9a60e9479c76b0e
BINTANG-XD <94216075+bot-85@users.noreply.github.com>
2021-11-25 19:02:15 +0700

    Update wow.py

--- wow.py
+++ wow.py
@@ -114,7 +114,7 @@ logo = (f"""{C}    _____           __   __ ___  _____
 /_____/         /_/  /_/_____/_/{Q}       
 
 [><] Athour by   : BINTANG-XD
-"%(H,N))[><] %sStatus      : Premium%s"""
+[><] %sStatus      : Premium""")
 
 loop = 0
 ok = []
