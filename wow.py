851112599b3903df134892133e0629cfc5ac3dec
BINTANG-XD <94216075+bot-85@users.noreply.github.com>
2021-11-25 18:54:53 +0700

    Update wow.py

--- wow.py
+++ wow.py
@@ -114,7 +114,7 @@ logo = (f"""{C}    _____           __   __ ___  _____
 /_____/         /_/  /_/_____/_/{Q}       
 
 [><] Athour by   : BINTANG-XD
-[><] YT Chanel.  : BINTANG XD""")
+     print(" [><] Status      : %sPremium %s"%(H,N))""")
 
 loop = 0
 ok = []
